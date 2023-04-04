85aa425acebdafeb537a6752cf53753621165d4a
Maxim Blinda <maximblinda@gmail.com>
2023-04-04 15:46:38 +0300

    Add project files.

---
+++ ДР_13_2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ДР_13_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.SetWindowSize(80, 40);
+            Console.CursorVisible = false;
+
+            Matrix instance;
+
+            for (int i = 0; i < 26; i++)
+            {   
+                instance = new Matrix(i * 3, true);
+                new Thread(instance.Move).Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ДР_13_3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ДР_13_3
+{
+    internal class Program
+    {
+        static int threadCount; 
+
+        static public void Recursion()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} say \"Hello!\"");
+
+            Thread.Sleep(500);
+
+            Thread thread = new Thread(Recursion);
+            threadCount++; 
+
+            thread.Name = "Thread " + threadCount;
+            thread.Start();
+        }
+
+        static void Main()
+        {
+            Thread thread = new Thread(Recursion);
+            thread.Name = "Thread " + threadCount;
+
+            thread.Start();
+        }
+    }
+}
